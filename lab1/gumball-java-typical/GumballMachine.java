63c70a43266ace96704ff9de605eebf17eab3b21
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2018-09-05 19:51:54 -0700

    Assignment submission for quiz question 1

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,33 +1,54 @@
+import java.util.ArrayList;
 
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private int gumball_cost;
+    private boolean can_deliver;
+    private int money_collected;
+    private ArrayList<Integer> accepted_coins;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int gumball_cost, ArrayList<Integer> accepted_coins)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.gumball_cost = gumball_cost;
+        this.can_deliver = false;
+        this.accepted_coins = accepted_coins;
     }
 
     public void insertQuarter(int coin)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    	if (this.accepted_coins.contains(coin)) {
+    		this.money_collected +=  coin;
+    		System.out.println("Money collected is : " + this.money_collected);
+    	}
+    	else {
+    		System.out.println("Insert valid coin");
+    	}
+    }
+    
+    public void canDeliver() {
+    	if (this.money_collected >=  this.gumball_cost) {
+    		this.can_deliver = true;
+    	}
+    	else {
+    		this.can_deliver = false;
+    	}
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	System.out.println("Total Money collected till now: " + this.money_collected);
+    	this.canDeliver();
+    	if ( this.can_deliver )
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
+    			this.can_deliver = false ;
+    			this.money_collected -= this.gumball_cost;
     			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
     		}
     		else
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -1,22 +1,21 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		GumballMachine gumballMachine = new GumballMachine(5, 50, new ArrayList<Integer>(Arrays.asList(5, 10, 25))) ;
 
-		System.out.println(gumballMachine);
+		gumballMachine.insertQuarter( 5 );
+		gumballMachine.insertQuarter( 5 );
+		gumballMachine.insertQuarter( 10 );
+		gumballMachine.insertQuarter( 25 );
+		gumballMachine.insertQuarter( 5 );
+		gumballMachine.turnCrank();
 
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
-
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
 	}
 }
